012325de89706a7ed2c8ab158ad633d804e687fb
Chris Jordan <cjordan@8thlight.com>
2016-09-23 14:34:10 +0100

    Refactors how triangles are drawn

--- app/initialize.js
+++ app/initialize.js
@@ -26,49 +26,98 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function start() {
   let shaderProgram = initShaders();
-  let buffer = initBuffers();
-  drawScene(buffer, shaderProgram);
+  let squareBuffer = initSquareBuffer();
+  let triangleBuffer = initTriangleBuffer();
+  clearScreen();
+  drawScene(triangleBuffer, squareBuffer, shaderProgram);
 }
 
-function drawScene(buffer, shaderProgram) {
-  clearScreen();
+function drawScene(triangleBuffer, squareBuffer, shaderProgram) {
+  const perspectiveMatrix = createPerspectiveMatrix();
+  let modelViewMatrix = createModelViewMatrix();
 
-  let pMatrix = mat4.create();
-  let mvMatrix = mat4.create();
-  mat4.perspective(pMatrix, 45, 640.0 / 480.0, 0.1, 100.0);
+  translate(modelViewMatrix, [-4.0, 2.0, -7.0]);
 
-  mat4.identity(mvMatrix);
-  mat4.translate(mvMatrix, mvMatrix, [-4.0, 2.0, -7.0]);
+  drawBuffer({ buffer: triangleBuffer,
+               shaderProgram: shaderProgram,
+               modelViewMatrix: modelViewMatrix,
+               perspectiveMatrix: perspectiveMatrix });
 
-  gl().vertexAttribPointer(shaderProgram.vertexPositionAttribute, buffer.itemSize, gl().FLOAT, false, 0, 0);
+  translate(modelViewMatrix, [3.0, 0.0, 0.0]);
 
-  setMatrixUniforms(pMatrix, mvMatrix, shaderProgram);
+  drawBuffer({ buffer: squareBuffer,
+               shaderProgram: shaderProgram,
+               modelViewMatrix: modelViewMatrix,
+               perspectiveMatrix: perspectiveMatrix });
+}
 
-  gl().drawArrays(gl().TRIANGLES, 0, buffer.numItems);
+function translate(matrix, vector) {
+  mat4.translate(matrix, matrix, vector);
+}
+
+function createModelViewMatrix() {
+  let matrix = mat4.create();
+  mat4.identity(matrix);
+
+  return matrix;
+}
+
+function createPerspectiveMatrix() {
+  let matrix = mat4.create();
+  mat4.perspective(matrix, 45, 640.0 / 480.0, 0.1, 100.0);
+  return matrix;
+}
+
+function drawBuffer({ buffer, shaderProgram, modelViewMatrix, perspectiveMatrix }) {
+  let index = shaderProgram.vertexPositionAttribute;
+  let stepSize = buffer.itemSize;
+  let type = gl().FLOAT;
+  let normalized = false;
+  let stride = 0;
+  let offset = 0;
+  let startingIndex = 0;
+
+  gl().bindBuffer(gl().ARRAY_BUFFER, buffer);
+  gl().vertexAttribPointer(index, stepSize, type, normalized, stride, offset);
+  setMatrixUniforms(perspectiveMatrix, modelViewMatrix, shaderProgram);
+  gl().drawArrays(gl().TRIANGLE_STRIP, startingIndex, buffer.numItems);
 }
 
 function clearScreen() {
   gl().clear(gl().COLOR_BUFFER_BIT | gl().DEPTH_BUFFER_BIT);
 }
 
-function setMatrixUniforms(pMatrix, mvMatrix, shaderProgram) {
-  gl().uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
-  gl().uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+function setMatrixUniforms(perspectiveMatrix, modelViewMatrix, shaderProgram) {
+  let transposeMatrix = false;
+
+  gl().uniformMatrix4fv(shaderProgram.pMatrixUniform, transposeMatrix, perspectiveMatrix);
+  gl().uniformMatrix4fv(shaderProgram.mvMatrixUniform, transposeMatrix, modelViewMatrix);
 }
 
-function initBuffers() {
+function initTriangleBuffer() {
+  return createBufferWith([
+    0.0,  1.0,  0.0,
+    -1.0, -1.0,  0.0,
+    1.0, -1.0,  0.0
+  ]);
+}
+
+function initSquareBuffer() {
+  return createBufferWith([
+    -1.0, -1.0,  0.0,
+    -1.0,  1.0,  0.0,
+    1.0, 1.0,  0.0,
+    1.0, -1.0,  0.0,
+    -1.0, -1.0,  0.0,
+  ]);
+}
+
+function createBufferWith(vertices) {
   let buffer = gl().createBuffer();
   gl().bindBuffer(gl().ARRAY_BUFFER, buffer);
 
-  let vertices = [
-    0.0,  1.0,  0.0,
-    -1.0, -1.0,  0.0,
-    1.0, -1.0,  0.0,
-    0.0, -1.0,  0.0,
-  ];
-
-  buffer.itemSize = 2;
-  buffer.numItems = 3;
+  buffer.itemSize = 3;
+  buffer.numItems = vertices.length / 3;
 
   gl().bufferData(gl().ARRAY_BUFFER, new Float32Array(vertices), gl().STATIC_DRAW);
   return buffer;
